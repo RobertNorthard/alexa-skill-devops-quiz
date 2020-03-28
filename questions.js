25124e0d548e3ab7191f52d7fb1c6054af457e16
Robert A. Northard <robertnorthard@googlemail.com>
2020-03-28 13:39:26 +0000

    DevOps Trivia initialise

---
+++ questions.js
@@ -0,0 +1,40 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_GB: [
+    {
+      'What is Terraform?': [
+        'Infrastructure as code tool',
+        'Configuration Management tool',
+        'DevOps tool',
+        'It is magic',
+        'It manages cloud resources',
+        'It is popular on DevOps CVs',
+      ],
+    },
+    {
+      'What is Jenkins? ': [
+        'A tool to support CI / CD approach to software release management',
+        'It\'s old school',
+        'It uses Jenkinsfiles to define CI / CD pipelines',
+        'There is an enterprise version called CloudBees Jenkins',
+        'A Chef',
+        'A DevOps tool',
+      ],
+    },
+    {
+      'What is a pod security policy?': [
+        'Enables limiting the privileges available to docker container running inside a Kubernetes platform',
+        'A policy on a pod',
+        'Does not exist',
+        'There is an enterprise version called CloudBees Jenkins',
+        'There is a PSP admission controller in Kubernetes',
+        'People often forget to use PSP',
+      ],
+    },
+  ],
+};
